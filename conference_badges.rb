a2f1df6b93e71f40cecf1bdd6facba92f389ee38
Gerardo Rapeta <g3r4rdo.r@gmail.com>
2023-06-15 10:19:34 -0700

    commit tree 3aa403af3b004bcfb301f4fafcb7cec442d5ba77

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name,room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |assignment|
+        puts assignment
+    end
+end
